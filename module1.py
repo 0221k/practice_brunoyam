f2cff896eafc90aa78caf9f122a032a98b5654ad
0221k <YOU@EXAMPLE.com>
2024-10-16 01:47:53 +0300

    third task

--- module1.py
+++ module1.py
@@ -7,3 +7,11 @@ dist = 109
 speed = int(input('speed: '))
 time = int(input('hours: '))
 print((speed * time) % dist)
+
+# m1, p1, lvl3
+num1 = float(input('first number: '))
+num2 = float(input('second number: '))
+if num1 > num2:
+	print(num1)
+else:
+	print(num2)
