9c1499aeec755d7931a63cb372d9651918261090
0221k <YOU@EXAMPLE.com>
2024-10-16 01:48:49 +0300

    4th, 5th, 6th tasks

--- module1.py
+++ module1.py
@@ -15,3 +15,36 @@ if num1 > num2:
 	print(num1)
 else:
 	print(num2)
+
+# m1, p2, lvl1
+first_number = int(input('first number: '))
+second_number = int(input('second number: '))
+third_number = int(input('third number: '))
+if first_number == second_number:
+	counter = 2
+	if first_number == third_number:
+		counter = 3
+elif second_number == third_number:
+	counter = 2
+elif third_number == first_number:
+	counter = 2
+else:
+	counter = 0
+print(counter)
+
+# m1, p2, lvl2
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+
+if ((x1 == x2) & (y1 != y2)) or ((x1 != x2) & (y1 == y2)):
+	print('YES')
+else:
+	print('NO')
+
+# m1, p2, lvl3
+password = ''
+while len(password) < 8:
+	password = input()
+print(password)
