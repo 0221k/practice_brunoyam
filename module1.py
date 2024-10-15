a82c037c46f3f0fe1d57ea6597a3f033ebaaf0a4
0221k <YOU@EXAMPLE.com>
2024-10-16 01:47:17 +0300

    second task

--- module1.py
+++ module1.py
@@ -1,3 +1,9 @@
 # m1, p1, lvl1
 value = (5 + ((7 * 5) / 8)) / (3 ** 5)
 print(value)
+
+# m1, p1, lvl2
+dist = 109
+speed = int(input('speed: '))
+time = int(input('hours: '))
+print((speed * time) % dist)
